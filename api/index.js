013b1dec04b9812242938923263c17c2a9f6a808
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 21:25:23 -0300

    chore: update shareCode to ZWKWCJL0SFW

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=RA4C3KNXG2Y&afp_campaign=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=ZWKWCJL0SFW&afp_campaign=tapi100' });
     res.end();
   }
