a608fc3d617025a40a1d0f2697cb18927e1da82c
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 23:06:59 -0300

    chore: shareCode ZA1FSKCMGXP -> QC7T89KMRI8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=ZA1FSKCMGXP&afp_campaign=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=QC7T89KMRI8&afp_campaign=tp' });
     res.end();
   }
